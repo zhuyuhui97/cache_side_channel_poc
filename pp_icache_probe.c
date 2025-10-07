b2aee795576f0676f2ac43e6149df87ea5b17a37
ZHU Yuhui <zhuyh97@gmail.com>
2025-10-08 00:12:27 +0200

    pp_icache_probe: major update

--- pp_icache_probe.c
+++ pp_icache_probe.c
@@ -5,24 +5,24 @@
 
 #include <argp.h>
 #include <assert.h>
+#include <fcntl.h> /* open */
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <time.h>
 #include <unistd.h>
-#include <fcntl.h> /* open */
 #include <unistd.h> /* pread, sysconf */
-
 
 #define NR_BPU_ACTV 4
 #define NR_BPU_TRAIN 32
 #define OFFSET_PROBE_INPAGE 0x2ca0
 #define FILL_BHB_LEN 600
+#define TRAMP_BITS_DEFAULT 26 // 64MB
 #define __NOP(x, rsh)                                                          \
     asm volatile(".rept " MACRO_TO_STR(x >> rsh) "\n nop\n .endr")
 #define NOP(x) __NOP(x, 0)
@@ -36,6 +36,8 @@
 #define OPS_BARRIER(x)                                                         \
     asm volatile("mfence");                                                    \
     NOP(x);
+#define FLUSH_ICACHE(p)                                                        \
+    asm volatile("clflush (%0)" ::"r"(test_target));
 #define OPCODE_RET (0xC3C3C3C3UL)
 #define NR_OPCODE_ALIGN 0
 #elif defined(__aarch64__)
@@ -43,6 +45,8 @@
     asm volatile("dsb sy");                                                    \
     asm volatile("isb");                                                       \
     NOP(x);
+#define FLUSH_ICACHE(p)                                                        \
+    asm volatile("ic ivau, %0\n dc civac, %0" ::"r"(test_target));
 #define OPCODE_RET (0xD65F03C0UL)
 #define NR_OPCODE_ALIGN 2
 #endif
@@ -50,32 +54,53 @@
 const bool dont_br = false, do_br = true;
 
 uint64_t os_page_size;
-uint64_t tramp_size = (64 << 20);
-// uint64_t cache_ways = 32;
-// uint64_t cache_idx_bits = 16;
-// uint64_t cache_ways = 16;
-// uint64_t cache_idx_bits = 10;
+uint64_t pid = -1;
+void* tramp_base = NULL;
+uint64_t tramp_bits = TRAMP_BITS_DEFAULT;
+uint64_t tramp_size = (1 << TRAMP_BITS_DEFAULT);
 uint64_t cache_ways = 16;
 uint64_t cache_idx_bits = 20;
 uint64_t cache_line_sz = 64;
+uint64_t offset_dbg_probe = OFFSET_PROBE_INPAGE;
+bool do_eviction = false;
 void *ret_tramp;
 
 static struct argp_option options[] = {
+    {"tramp", 't', "TRAMPOLINE_BASE", 0, "Base address of trampoline."},
     {"ways", 'w', "CACHE_WAYS", 0, "Number of ways."},
     {"bits", 'b', "CACHE_IDX_BITS", 0, "Number of index bits."},
     {"line", 'l', "CACHE_LINE_SZ", 0, "Size of cache line, default 64b."},
+    {"size", 's', "TRAMPOLINE_BITS", 0,
+     "Bits of address span of the RET trampoline, size=(1<<TRAMPOLINE_BITS)."},
+    {"offset", 'o', "OFFSET", 0, "DEBUG: test offset of P+P probe"},
+    {"evict", 'e', NULL, 0, "DEBUG: do eviction"},
     {0}};
+
+inline uint64_t read_cycles();
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     switch (key) {
+    case 't':
+        tramp_base = (void *)strtoull(arg, NULL, 0);
+        break;
     case 'w':
-        cache_ways = strtoul(arg, NULL, 0);
+        cache_ways = strtoull(arg, NULL, 0);
         break;
     case 'b':
-        cache_idx_bits = strtoul(arg, NULL, 0);
+        cache_idx_bits = strtoull(arg, NULL, 0);
         break;
     case 'l':
-        cache_line_sz = strtoul(arg, NULL, 0);
+        cache_line_sz = strtoull(arg, NULL, 0);
+        break;
+    case 's':
+        tramp_bits = strtoull(arg, NULL, 0);
+        tramp_size = 1 << tramp_bits;
+        break;
+    case 'o':
+        offset_dbg_probe = strtoull(arg, NULL, 0);
+        break;
+    case 'e':
+        do_eviction = true;
         break;
     default:
         return ARGP_ERR_UNKNOWN;
@@ -101,7 +126,8 @@ void *create_exec_tramp(uint32_t size, void *base) {
 
 void *create_ret_tramp(uint32_t size, void *base) {
     void *buf = create_exec_tramp(size, base);
-    memset(buf, (int)(OPCODE_RET & 0xFF), size);
+    for (void *_cursor=buf; _cursor<(buf+size); _cursor+=4)
+        *(uint32_t*)(_cursor) = OPCODE_RET;
     __builtin___clear_cache((char *)buf, (char *)buf + size);
     return buf;
 }
@@ -122,12 +148,24 @@ void fill_bhb(void) {
     }
 }
 
-#define NANOSECONDS_PER_SECOND 1000000000L
 uint64_t read_cycles() {
+#if !defined(TIMER_HW)
+#define NANOSECONDS_PER_SECOND 1000000000L
     struct timespec tp;
     clockid_t clk_id = CLOCK_REALTIME;
     clock_gettime(clk_id, &tp);
     return (uint64_t)((tp.tv_sec * NANOSECONDS_PER_SECOND) + tp.tv_nsec);
+#else
+#if defined(__x86_64__)
+    register uint64_t cnt_new_lo, cnt_new_hi;
+    asm volatile("rdtsc\n" : "=a" (cnt_new_lo), "=d" (cnt_new_hi) ::);
+    return ((cnt_new_hi<<32)|cnt_new_lo);
+#elif defined(__aarch64__)
+    register uint64_t cntr; \
+    asm volatile("mrs %0, pmccntr_el0" : "=r"(cntr)); \
+    return cntr;
+#endif
+#endif
 }
 
 /**
@@ -141,7 +179,6 @@ uint64_t measure_br_latency(bool *br_cond, register void *target) {
         OPS_BARRIER(8);
         t = read_cycles();
         CALL_ADDR(target);
-        OPS_BARRIER(8);
         t = read_cycles() - t;
     }
     return t;
@@ -161,36 +198,36 @@ void test_icache_latency(uint64_t *fast, uint64_t *slow) {
     // test branch latency when target in icache
     // TODO: MAGIC NUMBER HERE
     void *test_target = ret_tramp + 0xc20;
+    printf("Test addr: %p\n", test_target);
+    fflush(stdout);
     for (int i = 0; i < 4; i++)
         *fast = measure_br_latency((bool *)&do_br, test_target);
     // test branch latency when target not in icache
-#if defined(__x86_64__)
-    asm volatile("clflush (%0)" ::"r"(test_target));
-#elif defined(__aarch64__)
-    __builtin___clear_cache((char *)ret_tramp, (char *)ret_tramp + tramp_size);
-#endif
+    FLUSH_ICACHE(test_target);
     OPS_BARRIER(0x20);
     *slow = measure_br_latency((bool *)&do_br, test_target);
 }
 
-void get_evset(void *i_target, void *i_base, uint64_t i_len,
-               uint64_t i_addr_bits, uint64_t *o_n, void *o_evset[]) {
+void **get_evset(void *i_target, void *i_base, uint64_t i_len,
+               uint64_t i_addr_bits, uint64_t *o_n) {
     uint64_t stride = 1 << i_addr_bits;
     uint64_t mask = stride - 1;
     uint64_t index = (uint64_t)i_target & mask;
-    uint64_t base_unaligned = -(((uint64_t)i_base & mask) != 0); // -1 when unaligned
+    uint64_t base_unaligned =
+        -(((uint64_t)i_base & mask) != 0); // -1 when unaligned
     uint64_t cursor =
         (((uint64_t)i_base & ~mask) | index) + (base_unaligned & stride);
     uint64_t end = (uint64_t)i_base + i_len;
-    assert(((uint64_t)i_target<cursor)||(uint64_t)i_target>end);
+    assert(((uint64_t)i_target < cursor) || (uint64_t)i_target > end);
     uint64_t n = (1 & base_unaligned) + ((end - cursor) >> i_addr_bits);
     *o_n = n;
-    *o_evset = malloc(n * sizeof(void *));
+    void **o_evset = malloc(n * sizeof(void *));
     for (int i = 0; i < n; i++) {
-        assert(cursor<end);
+        assert(cursor < end);
         o_evset[i] = (void *)cursor;
         cursor += stride;
     }
+    return o_evset;
 }
 
 typedef struct {
@@ -201,24 +238,27 @@ typedef struct {
     unsigned int present : 1;
 } PagemapEntry;
 
-/* Parse the pagemap entry for the given virtual address.
+/*
  * https://github.com/cirosantilli/linux-kernel-module-cheat/blob/25f9913e0c1c5b4a3d350ad14d1de9ac06bfd4be/kernel_module/user/common.h
+ */
+
+/* Parse the pagemap entry for the given virtual address.
  *
  * @param[out] entry      the parsed entry
  * @param[in]  pagemap_fd file descriptor to an open /proc/pid/pagemap file
  * @param[in]  vaddr      virtual address to get entry for
  * @return 0 for success, 1 for failure
  */
-int pagemap_get_entry(PagemapEntry *entry, int pagemap_fd, uintptr_t vaddr)
-{
+int pagemap_get_entry(PagemapEntry *entry, int pagemap_fd, uintptr_t vaddr) {
     size_t nread;
     ssize_t ret;
     uint64_t data;
 
     nread = 0;
     while (nread < sizeof(data)) {
-        ret = pread(pagemap_fd, ((uint8_t*)&data) + nread, sizeof(data) - nread,
-                (vaddr / sysconf(_SC_PAGE_SIZE)) * sizeof(data) + nread);
+        ret =
+            pread(pagemap_fd, ((uint8_t *)&data) + nread, sizeof(data) - nread,
+                  (vaddr / sysconf(_SC_PAGE_SIZE)) * sizeof(data) + nread);
         nread += ret;
         if (ret <= 0) {
             return 1;
@@ -239,29 +279,30 @@ int pagemap_get_entry(PagemapEntry *entry, int pagemap_fd, uintptr_t vaddr)
  * @param[in] vaddr virtual address to get entry for
  * @return 0 for success, 1 for failure
  */
-int virt_to_phys_user(uintptr_t *paddr, pid_t pid, uintptr_t vaddr)
-{
-	char pagemap_file[BUFSIZ];
-	int pagemap_fd;
+int virt_to_phys_user(uintptr_t *paddr, pid_t pid, uintptr_t vaddr) {
+    char pagemap_file[BUFSIZ];
+    int pagemap_fd;
 
-	snprintf(pagemap_file, sizeof(pagemap_file), "/proc/%ju/pagemap", (uintmax_t)pid);
-	pagemap_fd = open(pagemap_file, O_RDONLY);
-	if (pagemap_fd < 0) {
-		return 1;
-	}
-	PagemapEntry entry;
-	if (pagemap_get_entry(&entry, pagemap_fd, vaddr)) {
-		return 1;
-	}
-	close(pagemap_fd);
-	*paddr = (entry.pfn * sysconf(_SC_PAGE_SIZE)) + (vaddr % sysconf(_SC_PAGE_SIZE));
-	return 0;
+    snprintf(pagemap_file, sizeof(pagemap_file), "/proc/%ju/pagemap",
+             (uintmax_t)pid);
+    pagemap_fd = open(pagemap_file, O_RDONLY);
+    if (pagemap_fd < 0) {
+        return 1;
+    }
+    PagemapEntry entry;
+    if (pagemap_get_entry(&entry, pagemap_fd, vaddr)) {
+        return 1;
+    }
+    close(pagemap_fd);
+    *paddr =
+        (entry.pfn * sysconf(_SC_PAGE_SIZE)) + (vaddr % sysconf(_SC_PAGE_SIZE));
+    return 0;
 }
 
 void test_evict() {
     void *test_base = ret_tramp;
     void *ev_base = ret_tramp + (tramp_size >> 1);
-    void *test_cursor = test_base + OFFSET_PROBE_INPAGE;
+    void *test_cursor = test_base + offset_dbg_probe;
     // load *test_cursor to L1i$
     CALL_ADDR(test_cursor);
     // let's try evicting *test_cursor
@@ -275,59 +316,49 @@ void test_evict() {
     printf("%llu\n", (unsigned long long)lat);
 }
 
-void test_evict2() {
-    void *test_base = ret_tramp;
-    void *ev_base = ret_tramp + (tramp_size >> 1);
-    void *test_cursor = test_base + OFFSET_PROBE_INPAGE;
-    void **evset = malloc(cache_ways * sizeof(void *));
-    uint64_t *latency = malloc(cache_ways * sizeof(uint64_t));
-    for (int i = 0; i < cache_ways; i++) {
-        evset[i] = ev_base + OFFSET_PROBE_INPAGE + (i << cache_idx_bits);
+void test_evict2(void *test_cursor, void *ev_base, uint64_t ev_size, uint64_t nr_ev) {
+    uint64_t nr_available_ev;
+    void **evset = get_evset(test_cursor, ev_base, ev_size, cache_idx_bits, &nr_available_ev);
+    assert(nr_available_ev>=nr_ev);
+    uint64_t *latency = malloc(nr_ev * sizeof(uint64_t));
+
+    for (int i = 0; i < nr_ev; i++) {
+        for (int j = 0; j < 8; j++) CALL_ADDR(evset[i]);
+        OPS_BARRIER(8);
     }
-    for (int i = 0; i < cache_ways; i++) {
-        for (int j = 0; j < 8; j++)
-        {
-            CALL_ADDR(evset[i]);
-            OPS_BARRIER(8);
+
+    if (do_eviction) {
+        test_cursor =evset[0]-(1<<cache_idx_bits);
+        // load *test_cursor to L1i$
+        for (int i = 0; i < 2; i++) {
+            CALL_ADDR(test_cursor);
         }
     }
-    // load *test_cursor to L1i$
-    // for (int i = 0; i < 32; i++) {
-    //     CALL_ADDR(test_cursor);
-    //     OPS_BARRIER(8);
-    // }
+    OPS_BARRIER(8);
 
-
-    // DEBUG: flush *evset[]
-//     for (int i = 0; i < cache_ways; i++) {
-// #if defined(__x86_64__)
-//         asm volatile("clflush (%0)" ::"r"(evset[i]));
-// #elif #elif defined(__aarch64__)
-//         __builtin___clear_cache((char *)evset[i],
-//                                 (char *)evset[i] + cache_line_sz);
-// #endif
-//     }
-
+#if defined(DBG_FLUSH_EVSET)
+    for (int i = 0; i < nr_ev; i++) {
+        FLUSH_ICACHE(evset[i]);
+    }
+#endif
 
     // let's detect who has been evicted by *test_cursor
-    for (int i = cache_ways - 1; i >= 0; i--) {
+    for (register int i = nr_ev - 1; i >= 0; i--) {
         OPS_BARRIER(8);
-        register int idx = (i + 0) % cache_ways;
-        latency[idx] = measure_br_latency((bool *)&do_br, evset[idx]);
+        latency[i] = measure_br_latency((bool *)&do_br, evset[i]);
     }
-    for (int i = 0; i < cache_ways; i++) {
-        printf("%llu ", (unsigned long long)latency[i]);
-    }
-    printf("\n");
 
-    printf("! = %p\n", test_cursor);
-    uint64_t pid = getpid();
-    for (int i = 0; i < cache_ways; i++) {
+    assert(pid!=-1);
+    {
+        uintptr_t paddr;
+        virt_to_phys_user(&paddr, pid, (uintptr_t)test_cursor);
+        printf("!\tv=%p\tp=%p\n", test_cursor, (void*)paddr);
+    }
+    for (int i = 0; i < nr_ev; i++) {
         uintptr_t paddr;
         virt_to_phys_user(&paddr, pid, (uintptr_t)evset[i]);
-        printf("%p %p\n", evset[i], paddr);
+        printf("%d\tv=%p\tp=%p\t%"PRIu64"\n", i, evset[i], (void*)paddr, latency[i]);
     }
-    // getchar();
 
     free(evset);
     free(latency);
@@ -335,6 +366,7 @@ void test_evict2() {
 
 void print_env() {
     printf("=== ENV VALUES ===\n");
+    printf("PID: %" PRIu64 "\n", pid);
     printf("Page size: %" PRIu64 "\n", os_page_size);
     printf("=== USER VALUES ===\n");
     printf("Cache ways: %" PRIu64 "\n", cache_ways);
@@ -344,27 +376,25 @@ void print_env() {
     printf("\n");
 }
 
-int main(int argc, char **argv) {
+void init(int argc, char **argv) {
     argp_parse(&argp, argc, argv, 0, 0, NULL);
+    assert(offset_dbg_probe<tramp_size);
     os_page_size = getpagesize();
+    pid = getpid();
     print_env();
+}
 
-    ret_tramp = create_ret_tramp(tramp_size, (void *)0x8003000);
+int main(int argc, char **argv) {
+    init(argc, argv);
+    ret_tramp = create_ret_tramp(tramp_size, tramp_base);
     assert(ret_tramp);
 
     uint64_t cache_fast, cache_slow;
     test_icache_latency(&cache_fast, &cache_slow);
-    printf("%llu %llu\n", (unsigned long long)cache_fast,
-           (unsigned long long)cache_slow);
+    printf("FAST:%"PRIu64" SLOW:%"PRIu64"\n", cache_fast, cache_slow);
 
-    test_evict2();
-
-    uint64_t nr_evset;
-    void **evset;
-    get_evset((void*)0xabc0000, (void*)0x1abc0000, 0x800000, 16, &nr_evset, evset);
-    // printf("%d\n", nr_evset);
-    // for (int i=0; i<nr_evset; i++)
-    // {
-    //     printf("%p\n", evset[i]);
-    // }
+    test_evict2(ret_tramp + offset_dbg_probe,
+                ret_tramp + (tramp_size >> 1),
+                tramp_size >> 1,
+                cache_ways);
 }
